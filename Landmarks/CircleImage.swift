2bfc63a7a5c2e28bfbb5f7b9988b715bb0630456
Sam Orgill <ssorgill@icloud.com>
2020-08-06 20:11:22 +0100

    Image and border

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Sam Orgill on 06/08/2020.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(
+                Circle()
+                    .stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
